300f660e81bdbf52093f36be47dea2acf43aa10d
averywallis <averybwallis@gmail.com>
2016-09-18 12:01:14 -0400

    Updated from Brython Server: 9/18/2016 12:01:14 PM

--- calculatepi.py
+++ calculatepi.py
@@ -1,24 +1,20 @@
 """
 calculatepi.py
 Author: Avery Wallis
-Credit: None
 
-Assignment:
-Write and submit a Python program that computes an approximate value of π by calculating the following sum:
-(see: https://github.com/HHS-IntroProgramming/Calculate-Pi/blob/master/README.md)
-This sum approaches the true value of π as n approaches ∞.
-Your program must ask the user how many terms to use in the estimate of π, how many decimal places, 
-then print the estimate using that many decimal places. Exactly like this:
-
-I will estimate pi. How many terms should I use? 100
-How many decimal places should I use in the result? 7
-The approximate value of pi is 3.1315929
-
-Note: remember that the printed value of pi will be an estimate!
+This was an experiment for Math Modeling and Fractions
 
 """
 import math
+
+
+
+
+
+
+"""
 n = int(input("I will estimate pi. How many terms should I use? "))
 decimals = int(input("How many decimal places should I use in the result? "))
 pi = float(4*(sum([((-1.0)**k)/(2*k+1) for k in range(0,n)])))
 print("The approximate value of pi is {0}".format(round(pi, decimals)))
+"""
