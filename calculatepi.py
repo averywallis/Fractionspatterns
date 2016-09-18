e1736ffec536df5c2cb170de715c84afcae97a75
averywallis <averybwallis@gmail.com>
2016-09-18 12:08:16 -0400

    Updated from Brython Server: 9/18/2016 12:08:16 PM

--- calculatepi.py
+++ calculatepi.py
@@ -7,8 +7,12 @@ This was an experiment for Math Modeling and Fractions
 """
 import math
 lists = []
-a = 1/2
-lists.append(a)
+a = 0
+b = 0
+for x in range(0,10):
+    a += 1
+    b = b
+    lists.append(a)
 print(lists)
 
 
