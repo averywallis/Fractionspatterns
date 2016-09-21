19fe3a2f5630dd5c75358188dc8418987c538b5a
averywallis <averybwallis@gmail.com>
2016-09-20 20:51:52 -0400

    Updated from Brython Server: 9/20/2016 8:51:53 PM

--- fractionspatterns.py
+++ fractionspatterns.py
@@ -5,13 +5,29 @@ Author: Avery Wallis
 This was an experiment for Math Modeling and Fractions
 
 """
+import math
+lista = []
+listb = []
+listc = []
+listd = []
+a = 0
+b = 0
+d = int(input("Largest Denominator?"))
+for x in range(0,d):
+    print(x)
+    a += 1
+    b = 1
+    c = a/b
+    lista.append(c)
 
-# w= int(input('Width of multiplication table: '))
-# h= int(input('Height of multiplication table: '))
-#for x in range(1,h+1):
-#    for y in range(1,w+1):
-#       print("{0: >3} ".format(x/y), end="")
-#    print()
+
+"""
+w= int(input('Width of multiplication table: '))
+h= int(input('Height of multiplication table: '))
+for x in range(1,h+1):
+    for y in range(1,w+1):
+       print("{0: >3} ".format(x/y), end="")
+    print()
 
 import math
 lista = []
@@ -60,7 +76,7 @@ print(listc)
 print(listd)
 
 
-"""
+
 n = int(input("I will estimate pi. How many terms should I use? "))
 decimals = int(input("How many decimal places should I use in the result? "))
 pi = float(4*(sum([((-1.0)**k)/(2*k+1) for k in range(0,n)])))
